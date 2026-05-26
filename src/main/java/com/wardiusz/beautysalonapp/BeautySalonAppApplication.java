e6f228aa273e215199a7fc37fed3ae62307b3fa2
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-05-26 21:58:46 +0200

    chore: initial commit

---
+++ src/main/java/com/wardiusz/beautysalonapp/BeautySalonAppApplication.java
@@ -0,0 +1,13 @@
+package com.wardiusz.beautysalonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySalonAppApplication.class, args);
+    }
+
+}
